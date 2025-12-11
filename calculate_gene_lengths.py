7b3aff017c8efb5df0838040012c61278ed7c0a8
Bereket Alemayehu Admasu <bereketalemayehua@gwmail.gwu.edu>
2025-12-11 00:56:07 -0500

    Update fasta_file comment for clarity

--- calculate_gene_lengths.py
+++ calculate_gene_lengths.py
@@ -1,4 +1,4 @@
-fasta_file = "TODO: Specify input file"
+fasta_file = "TODO: Specify output from *get_genes_fastas.py*"
 
 for record in SeqIO.parse(fasta_file, "fasta"):
     print(record.id, len(record.seq))
